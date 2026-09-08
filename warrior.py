e63853a9f1e1a156f7ea80987a338c5f3fb91956
Kevin Ying <90920995+KevinYing09@users.noreply.github.com>
2026-09-08 13:13:07 -0600

    Refactor Warrior class attack method and attributes

--- warrior.py
+++ warrior.py
@@ -7,15 +7,15 @@ class Warrior(Player):
 
     def __init__(self, name, x, y):
         super().__init__(name, x, y)
-        self.attack = 20
+        self.damage = 20
+        self.max_health = 125
+        self.max_health = 125
         self.speed = 8
 
 #This attack has extremely high base damage, but doesn't fare too well against enemies with a similar defense to your attack
-    def swingSword(self, opp_defense):
-        return 40 * self.attack//opp_defense + 1
+    def attack(self, opp_defense):
+        return 40 * self.damage//opp_defense + 1
 
     def draw(self, surface):
         rect = pygame.Rect(self.x - 20, self.y - 20, 40, 40)
         pygame.draw.rect(surface, (0, 240, 0), rect)
-
-    
