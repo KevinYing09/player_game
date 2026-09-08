9ce9493c244d5f3873b1f6f891e1d612dca9beb8
Kevin Ying <90920995+KevinYing09@users.noreply.github.com>
2026-09-08 13:12:51 -0600

    Refactor spell casting to use attack method

--- mage.py
+++ mage.py
@@ -10,8 +10,8 @@ class Mage(Player):
         self.defense = 20
 
 #This attack has a much higher minimum damage (20), but in return the increase from being stronger than the enemy is much less
-    def castSpell(self, opp_defense):
-        return 20 * self.attack/opp_defense + 20
+    def attack(self, opp_defense):
+        return 20 * self.damage/opp_defense + 25
 
     def draw(self, surface):
         pygame.draw.circle(surface, (240, 0, 0), (self.x, self.y), 20)
